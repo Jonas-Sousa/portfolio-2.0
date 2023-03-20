c6a464041f37e53e1991af9b2ada8ba1a9e1a608
Jonas sousa <jonassousagtr@gmail.com>
2023-03-19 21:15:10 -0300

    Feat: Adicionando um array de objetos, para preencher o modal dinâmicamente.

---
+++ js/arrayCursos.js
@@ -0,0 +1,67 @@
+const javascript = [
+    {
+        titulo: "Javascript :",
+        subTitulo: "Crie páginas dinâmicas",
+        tempoCurso: 10
+    },
+    {
+        titulo: "Javascript :",
+        subTitulo: "Manipulando o DOM",
+        tempoCurso: 6
+    },
+    {
+        titulo: "Javascript :",
+        subTitulo: "Armazenamento de dados no navegador",
+        tempoCurso: 6
+    },
+    {
+        titulo: "Javascript :",
+        subTitulo: "Criando requisições",
+        tempoCurso: 8
+    },
+    {
+        titulo: "Javascript :",
+        subTitulo: "Validando formulários",
+        tempoCurso: 8
+    },
+    {
+        titulo: "Javascript :",
+        subTitulo: "Validações e reconhecimento de voz",
+        tempoCurso: 6
+    },
+    {
+        titulo: "Javascript :",
+        subTitulo: "Consumindo e tratando dados de uma AP",
+        tempoCurso: 6
+    },
+    {
+        titulo: "Javascript :",
+        subTitulo: "Métodos de array",
+        tempoCurso: 8
+    },
+    {
+        titulo: "Javascript :",
+        subTitulo: "Manipulando objetos",
+        tempoCurso: 8
+    },
+    {
+        titulo: "Javascript :",
+        subTitulo: "Desenvolva um jogo e pratique lógica de programação",
+        tempoCurso: 16
+    },
+    {
+        titulo: "Javascript :",
+        subTitulo: "Algoritmos de ordenação",
+        tempoCurso: 8
+    },
+    {
+        titulo: "Javascript :",
+        subTitulo: "Aprofundando em algoritmos de ordenação e busca",
+        tempoCurso: 10
+    },
+    {
+        titulo: "Javascript :",
+        subTitulo: "Programando na linguagem da web",
+        tempoCurso: 20
+    }
+]
