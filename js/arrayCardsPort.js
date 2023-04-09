89b4cf6c1171a9e101f7fe436e18501fd2644c76
Jonas sousa <jonassousagtr@gmail.com>
2023-04-09 09:26:25 -0300

    Atualizando a lista de portfolios.

--- js/arrayCardsPort.js
+++ js/arrayCardsPort.js
@@ -13,4 +13,39 @@ export const cards = [
         linkSite: "https://organo-ashen-nine.vercel.app",
         linkGithub: "https://github.com/Jonas-Sousa/organo"
     },
+    {
+        imagem: "./img/teste-port.png",
+        titulo: "Facilista",
+        descricao: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati a corporis",
+        linkSite: "https://jonas-sousa.github.io/Facilista/",
+        linkGithub: "https://github.com/Jonas-Sousa/Facilista"
+    },
+    {
+        imagem: "./img/teste-port.png",
+        titulo: "Tabela IMC",
+        descricao: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati a corporis",
+        linkSite: "https://jonas-sousa.github.io/tabela-nutricao/",
+        linkGithub: "https://github.com/Jonas-Sousa/tabela-nutricao"
+    },
+    {
+        imagem: "./img/teste-port.png",
+        titulo: "Monibanck",
+        descricao: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati a corporis",
+        linkSite: "https://jonas-sousa.github.io/monibanck/pages/abrir-conta-form.html",
+        linkGithub: "https://github.com/Jonas-Sousa/monibanck"
+    },
+    {
+        imagem: "./img/teste-port.png",
+        titulo: "Bytebanck",
+        descricao: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati a corporis",
+        linkSite: "https://jonas-sousa.github.io/bytebanck/",
+        linkGithub: "https://github.com/Jonas-Sousa/bytebanck"
+    },
+    {
+        imagem: "./img/teste-port.png",
+        titulo: "Desafio Elo7",
+        descricao: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati a corporis",
+        linkSite: "https://desafio-elo7-12.vercel.app",
+        linkGithub: "https://github.com/Jonas-Sousa/Desafio-Elo7"
+    },
 ]
