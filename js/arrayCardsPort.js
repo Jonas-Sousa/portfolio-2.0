e5a5afdd86e6b2011c591e0d713d2144bdde2a7a
Jonas sousa <jonassousagtr@gmail.com>
2023-04-09 08:52:23 -0300

    Refactor: Criei os arquivos 'cardsPort' e 'arrayCardsPort' para adicionar os cards de portfolio dinâmicamente no site, um arquivo tem os objetos dos dados dos cards(arrayCardsPort) e o outro pega esses dados e cria os cards dinâmicamente(cardsPort).

---
+++ js/arrayCardsPort.js
@@ -0,0 +1,16 @@
+export const cards = [
+    {
+        imagem: "./img/teste-port.png",
+        titulo: "Space",
+        descricao: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati a corporis",
+        linkSite: "https://space-gallery-pi.vercel.app",
+        linkGithub: "https://github.com/Jonas-Sousa/alura-space"
+    },
+    {
+        imagem: "./img/teste-port.png",
+        titulo: "Organo",
+        descricao: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati a corporis",
+        linkSite: "https://organo-ashen-nine.vercel.app",
+        linkGithub: "https://github.com/Jonas-Sousa/organo"
+    },
+]
